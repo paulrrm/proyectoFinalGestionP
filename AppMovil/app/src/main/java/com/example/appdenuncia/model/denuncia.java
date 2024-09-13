84af558208affa0494cbcd384d3fb29a17a1bedf
PAUL RUIZ <paulruizm@gmail.com>
2024-09-13 00:29:47 -0500

    cambio denunicas sin id

--- AppMovil/app/src/main/java/com/example/appdenuncia/model/denuncia.java
+++ AppMovil/app/src/main/java/com/example/appdenuncia/model/denuncia.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class denuncia {
 
-    private Long id;
+
 
     private Date fechalta;
 
@@ -25,13 +25,7 @@ public class denuncia {
     private placa placa;
 
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public Date getFechalta() {
         return fechalta;
